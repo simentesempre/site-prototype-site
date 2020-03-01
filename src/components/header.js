b1dbdf4aadb299aba0466a5ac60e6459c1f6d693
simentesempre <federico.portoghese@gmail.com>
2020-03-01 19:57:23 +0100

    Fixes color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `lightcoral`,
+      background: `darkcyan`,
       marginBottom: `1.45rem`,
     }}
   >
