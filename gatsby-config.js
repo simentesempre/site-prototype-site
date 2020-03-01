6f6bdd40cf45d1f3d5258414a0a01825823ac643
simentesempre <federico.portoghese@gmail.com>
2020-03-01 19:12:51 +0100

    Fixes env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL,
+        apiURL: process.env.API_URL || 'http://localhost:1337',
         queryLimit: 1000, // Default to 100
         contentTypes: [`post`, `user`],
       },
